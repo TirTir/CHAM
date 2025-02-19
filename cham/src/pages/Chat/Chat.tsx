03ecf59e917515c4f7fb7464ca909a4757d039ea
yujin <uj0791navercom@skuniv.ac.kr>
2025-02-20 00:19:27 +0900

    [REFACTOR] Data Structure
    - 데이터 JSON 파일 추가
    - 채팅 메시지 데이터 분리

--- cham/src/pages/Chat/Chat.tsx
+++ cham/src/pages/Chat/Chat.tsx
@@ -1,90 +1,41 @@
+import { useEffect, useState } from "react";
 import { UserOutlined } from "@ant-design/icons";
 import { Avatar } from "antd";
 import * as S from "./Chat.styled";
+import chatData from "./Data.json"; // JSON 파일 import
 
 export function Chat() {
+  const [messages, setMessages] = useState(chatData.chatData);
+
+  useEffect(() => {
+    // 메시지를 불러오고 상태 업데이트 (추후 API 연동 가능)
+    setMessages(chatData.chatData);
+  }, []);
+
   return (
     <S.MessagesContainer>
-      <S.DateLabelWrapper>
-        <S.DateLabel>2023-04-19</S.DateLabel>
-      </S.DateLabelWrapper>
+      {messages.map((chat) => (
+        <div key={chat.date}>
+          {/* 날짜 라벨 */}
+          <S.DateLabelWrapper>
+            <S.DateLabel>{chat.date}</S.DateLabel>
+          </S.DateLabelWrapper>
 
-      <S.MessageWrapper>
-        <S.AvatarWrapper>
-          <Avatar size="large" icon={<UserOutlined />} />
-        </S.AvatarWrapper>
-        <S.MessageContent>
-          <S.UserName>김유진</S.UserName>
-          <S.MessageTime>오후 11:59</S.MessageTime>
-          <S.MessageText>안녕하세요!</S.MessageText>
-        </S.MessageContent>
-      </S.MessageWrapper>
-
-      <S.MessageWrapper>
-        <S.AvatarWrapper>
-          <Avatar size="large" icon={<UserOutlined />} />
-        </S.AvatarWrapper>
-        <S.MessageContent>
-          <S.UserName>김유진</S.UserName>
-          <S.MessageText>좋은 하루 보내세요!</S.MessageText>
-          <S.MessageTime>오전 12:00</S.MessageTime>
-        </S.MessageContent>
-      </S.MessageWrapper>
-
-      <S.MessageWrapper>
-        <S.AvatarWrapper>
-          <Avatar size="large" icon={<UserOutlined />} />
-        </S.AvatarWrapper>
-        <S.MessageContent>
-          <S.UserName>김유진</S.UserName>
-          <S.MessageText>좋은 하루 보내세요!</S.MessageText>
-          <S.MessageTime>오전 12:00</S.MessageTime>
-        </S.MessageContent>
-      </S.MessageWrapper>
-
-      <S.MessageWrapper>
-        <S.AvatarWrapper>
-          <Avatar size="large" icon={<UserOutlined />} />
-        </S.AvatarWrapper>
-        <S.MessageContent>
-          <S.UserName>김유진</S.UserName>
-          <S.MessageText>좋은 하루 보내세요!</S.MessageText>
-          <S.MessageTime>오전 12:00</S.MessageTime>
-        </S.MessageContent>
-      </S.MessageWrapper>
-
-      <S.MessageWrapper>
-        <S.AvatarWrapper>
-          <Avatar size="large" icon={<UserOutlined />} />
-        </S.AvatarWrapper>
-        <S.MessageContent>
-          <S.UserName>김유진</S.UserName>
-          <S.MessageText>좋은 하루 보내세요!</S.MessageText>
-          <S.MessageTime>오전 12:00</S.MessageTime>
-        </S.MessageContent>
-      </S.MessageWrapper>
-
-      <S.MessageWrapper>
-        <S.AvatarWrapper>
-          <Avatar size="large" icon={<UserOutlined />} />
-        </S.AvatarWrapper>
-        <S.MessageContent>
-          <S.UserName>김유진</S.UserName>
-          <S.MessageText>좋은 하루 보내세요!</S.MessageText>
-          <S.MessageTime>오전 12:00</S.MessageTime>
-        </S.MessageContent>
-      </S.MessageWrapper>
-
-      <S.MessageWrapper>
-        <S.AvatarWrapper>
-          <Avatar size="large" icon={<UserOutlined />} />
-        </S.AvatarWrapper>
-        <S.MessageContent>
-          <S.UserName>김유진</S.UserName>
-          <S.MessageText>좋은 하루 보내세요!</S.MessageText>
-          <S.MessageTime>오전 12:00</S.MessageTime>
-        </S.MessageContent>
-      </S.MessageWrapper>
+          {/* 해당 날짜의 메시지 리스트 */}
+          {chat.messages.map((message) => (
+            <S.MessageWrapper key={message.id}>
+              <S.AvatarWrapper>
+                <Avatar size="large" icon={<UserOutlined />} />
+              </S.AvatarWrapper>
+              <S.MessageContent>
+                <S.UserName>{message.username}</S.UserName>
+                <S.MessageText>{message.text}</S.MessageText>
+                <S.MessageTime>{message.time}</S.MessageTime>
+              </S.MessageContent>
+            </S.MessageWrapper>
+          ))}
+        </div>
+      ))}
     </S.MessagesContainer>
   );
 }
